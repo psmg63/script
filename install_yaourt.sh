590f67a2cf84be6d434863caf158c5cacd43bcba
psmg63 <psmg@mail.ru>
2016-10-02 09:01:14 +0400

    initial commit

---
+++ install_yaourt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si --noconfirm
+cd ..
+rm -rf /package-query
+
+
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si --noconfirm
+cd ..
+rm -rf /yaourt
